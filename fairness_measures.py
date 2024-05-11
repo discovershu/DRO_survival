b93af58623e6df23387145b120e9fe90a743699a
Shu Hu <shu.hu@foxmail.com>
2024-05-11 11:22:50 +0800

    Add files via upload

--- fairness_measures.py
+++ fairness_measures.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sksurv.metrics import concordance_index_censored, concordance_index_ipcw
 from sksurv.util import Surv
+import torch
 
 
 # %% fairness metric: individual fairness
@@ -23,6 +24,32 @@ def individual_fairness(prediction, X):
     return R_beta_avg
 
 
+def individual_fairness_td(prediction, X):
+    F_I_score_list = []
+    eval_index = [int(np.percentile(range(prediction.shape[0]), 25)),
+                  int(np.percentile(range(prediction.shape[0]), 50)),
+                  int(np.percentile(range(prediction.shape[0]), 75))]
+    for index in eval_index:
+        HazardFunction = prediction[index]
+        # =============================================================================
+        #     normalizer = np.linalg.norm(model_prediction, 2) # L2 normalization
+        #     HazardFunction = HazardFunction/normalizer # L2 normalization
+        # =============================================================================
+        N = len(prediction[index])
+        R_beta = 0.0  # initialization of individual fairnessd
+        for i in range(len(prediction[index])):
+            for j in range(len(prediction[index])):
+                if j <= i:
+                    continue
+                else:
+                    distance = np.sqrt(sum((X[i] - X[j]) ** 2))  # euclidean distance
+                    R_beta = R_beta + max(0, (np.abs(HazardFunction[i] - HazardFunction[j]) - distance))
+        R_beta_avg = R_beta / (N * (N - 1))
+        F_I_score_list.append(R_beta_avg)
+    F_I_score = np.mean(np.asarray(F_I_score_list))
+    return F_I_score
+
+
 def individual_fairness_scale(prediction, X, scale):
     HazardFunction = np.exp(prediction)
     N = len(prediction)
@@ -36,6 +63,32 @@ def individual_fairness_scale(prediction, X, scale):
                 R_beta = R_beta + max(0, (np.abs(HazardFunction[i] - HazardFunction[j]) - scale * distance))
     R_beta_avg = R_beta / (N * (N - 1))
     return R_beta_avg
+
+
+def individual_fairness_scale_td(prediction, X, scale):
+    F_I_score_list = []
+    eval_index = [int(np.percentile(range(prediction.shape[0]), 25)),
+                  int(np.percentile(range(prediction.shape[0]), 50)),
+                  int(np.percentile(range(prediction.shape[0]), 75))]
+    for index in eval_index:
+        N = len(prediction[index])
+        mat_select = np.zeros((N, N))
+        rows, cols = np.triu_indices(N, k=1)
+        mat_select[rows, cols] = 1
+        mat_select = torch.from_numpy(mat_select)
+
+        diff_pred = torch.from_numpy(prediction[index]).unsqueeze(1) - torch.from_numpy(prediction[index]).unsqueeze(0)
+        distances_pred = torch.abs(diff_pred)
+
+        diff = torch.from_numpy(X).unsqueeze(1) - torch.from_numpy(X).unsqueeze(0)
+        distances_squared = torch.sum(torch.square(diff), dim=-1)
+        distances = torch.sqrt(distances_squared)
+
+        F_CI_score = torch.sum(torch.relu(distances_pred - scale * distances) * mat_select).detach().numpy()
+        F_CI_score = F_CI_score / (N * (N - 1))
+        F_I_score_list.append(F_CI_score)
+    F_I_score = np.mean(np.asarray(F_I_score_list))
+    return F_I_score
 
 
 def individual_fairness_scale_censoring(prediction, X, scale, event, time):
@@ -60,27 +113,33 @@ def individual_fairness_scale_censoring(prediction, X, scale, event, time):
 
 
 def individual_fairness_scale_censoring_td(prediction, X, scale, event, time):
+    c_ref_time = torch.from_numpy(time[event == False])
+    uc_ref_time = torch.from_numpy(time[event == True])
+    c_surv = torch.from_numpy(prediction[:, event == False])
+    uc_surv = torch.from_numpy(prediction[:, event == True])
+    c_X = torch.from_numpy(X[event == False])
+    uc_X = torch.from_numpy(X[event == True])
     censoring_count = np.sum(event == False)
     uncensored_count = np.sum(event == True)
     F_CI_score_list = []
     eval_index = [int(np.percentile(range(prediction.shape[0]), 25)),
                   int(np.percentile(range(prediction.shape[0]), 50)),
                   int(np.percentile(range(prediction.shape[0]), 75))]
+
     for index in eval_index:
-        F_CI_score_sum = 0.0
-        for i in range(len(prediction[0])):
-            for j in range(len(prediction[0])):
-                if event[i] == False:
-                    if event[j] == True:
-                        if time[i] <= time[j]:
-                            distance = np.sqrt(sum((X[i] - X[j]) ** 2))
-                            F_CI_score_sum = F_CI_score_sum + max(0.0, (
-                                    np.abs(prediction[index][i] - prediction[index][j]) - scale * distance))
-        if censoring_count != 0 and uncensored_count != 0:
-            F_CI_score_avg = F_CI_score_sum / (censoring_count * uncensored_count)
-        else:
-            F_CI_score_avg = F_CI_score_sum
-        F_CI_score_list.append(F_CI_score_avg)
+        diff_time = c_ref_time.unsqueeze(1) - uc_ref_time.unsqueeze(0)
+        mat_all = (diff_time <= 0).type(torch.float)
+
+        diff_pred = c_surv[index, :].unsqueeze(1) - uc_surv[index, :].unsqueeze(0)
+        distances_pred = torch.abs(diff_pred)
+
+        diff = c_X.unsqueeze(1) - uc_X.unsqueeze(0)
+        distances_squared = torch.sum(torch.square(diff), dim=-1)
+        distances = torch.sqrt(distances_squared)
+
+        F_CI_score = torch.sum(torch.relu(distances_pred - scale * distances) * mat_all).detach().numpy()
+        F_CI_score = F_CI_score / (censoring_count * uncensored_count)
+        F_CI_score_list.append(F_CI_score)
     F_CI_score = np.mean(np.asarray(F_CI_score_list))
     return F_CI_score
 
@@ -106,6 +165,35 @@ def group_fairness(prediction, S):
 
     group_fairness = np.max(np.abs(avg_h_ratio_group - avg_h_ratio))
     return group_fairness
+
+
+def group_fairness_td(prediction, S):
+    F_G_score_list = []
+    eval_index = [int(np.percentile(range(prediction.shape[0]), 25)),
+                  int(np.percentile(range(prediction.shape[0]), 50)),
+                  int(np.percentile(range(prediction.shape[0]), 75))]
+    for index in eval_index:
+        h_ratio = prediction[index]
+        unique_group = np.unique(S)
+        avg_h_ratio = sum(h_ratio) / len(h_ratio)
+
+        numClasses = 2
+        concentrationParameter = 1.0
+        dirichletAlpha = concentrationParameter / numClasses
+
+        h_ratio_group = np.zeros((len(unique_group)))
+        group_total = np.zeros((len(unique_group)))
+
+        for i in range(len(h_ratio)):
+            h_ratio_group[S[i]] = h_ratio_group[S[i]] + h_ratio[i]
+            group_total[S[i]] = group_total[S[i]] + 1
+
+        avg_h_ratio_group = (h_ratio_group + dirichletAlpha) / (group_total + concentrationParameter)
+
+        group_fairness = np.max(np.abs(avg_h_ratio_group - avg_h_ratio))
+        F_G_score_list.append(group_fairness)
+    F_G_score = np.mean(np.asarray(F_G_score_list))
+    return F_G_score
 
 
 def group_fairness_censoring(prediction, S, X, scale, event, time):
@@ -135,27 +223,68 @@ def group_fairness_censoring(prediction, S, X, scale, event, time):
 
 def group_fairness_censoring_td(prediction, S, X, scale, event, time):
     unique_group = np.unique(S)
-    F_CG_score_sum_group = np.zeros((len(unique_group)))
     censoring_count = np.sum(event == False)
     uncensored_count = np.sum(event == True)
-    F_CG_score_list = []
+    F_CG_score = 0
     eval_index = [int(np.percentile(range(prediction.shape[0]), 25)),
                   int(np.percentile(range(prediction.shape[0]), 50)),
                   int(np.percentile(range(prediction.shape[0]), 75))]
-    for index in eval_index:
-        for i in range(len(prediction[0])):
-            for j in range(len(prediction[0])):
-                if event[i] == False and event[j] == True and S[i] == S[j] and time[i] <= time[j]:
-                    distance = np.sqrt(sum((X[i] - X[j]) ** 2))
-                    F_CG_score_sum_group[int(S[i])] = F_CG_score_sum_group[int(S[i])] + max(0.0, (
-                            np.abs(prediction[index][i] - prediction[index][j]) - scale * distance))
-        if censoring_count != 0 and uncensored_count != 0:
-            F_CG_score_avg = np.sum(F_CG_score_sum_group) / (censoring_count * uncensored_count)
-        else:
-            F_CG_score_avg = 0.0
-        F_CG_score_list.append(F_CG_score_avg)
-    F_CG_score = np.mean(np.asarray(F_CG_score_list))
-    return F_CG_score
+
+    for group in unique_group:
+        group_surv = prediction[:, S == group]
+        group_ref_event = event[S == group]
+        group_ref_time = time[S == group]
+        c_surv = torch.from_numpy(group_surv[:, group_ref_event == 0])
+        c_ref_time = torch.from_numpy(group_ref_time[group_ref_event == 0])
+        uc_surv = torch.from_numpy(group_surv[:, group_ref_event == 1])
+        uc_ref_time = torch.from_numpy(group_ref_time[group_ref_event == 1])
+
+        grou_X = X[S == group]
+        c_X = torch.from_numpy(grou_X[group_ref_event == 0])
+        uc_X = torch.from_numpy(grou_X[group_ref_event == 1])
+
+        F_CG_score_group = 0
+
+        for index in eval_index:
+            diff_time = c_ref_time.unsqueeze(1) - uc_ref_time.unsqueeze(0)
+            mat_all = (diff_time <= 0).type(torch.float)
+
+            diff_pred = c_surv[index, :].unsqueeze(1) - uc_surv[index, :].unsqueeze(0)
+            distances_pred = torch.abs(diff_pred)
+
+            diff = c_X.unsqueeze(1) - uc_X.unsqueeze(0)
+            distances_squared = torch.sum(torch.square(diff), dim=-1)
+            distances = torch.sqrt(distances_squared)
+
+            F_CG_score_group = F_CG_score_group + torch.sum(
+                torch.relu(distances_pred - scale * distances) * mat_all).detach().numpy()
+
+        F_CG_score = F_CG_score + F_CG_score_group
+
+    F_CG_score_avg = F_CG_score / (censoring_count * uncensored_count * len(eval_index))
+    return F_CG_score_avg
+
+    # unique_group = np.unique(S)
+    # F_CG_score_sum_group = np.zeros((len(unique_group)))
+    # censoring_count = np.sum(event == False)
+    # uncensored_count = np.sum(event == True)
+    # F_CG_score_list = []
+    # eval_index = [int(np.percentile(range(prediction.shape[0]), 25)), int(np.percentile(range(prediction.shape[0]), 50)),
+    #               int(np.percentile(range(prediction.shape[0]), 75))]
+    # for index in eval_index:
+    #     for i in range(len(prediction[0])):
+    #         for j in range(len(prediction[0])):
+    #             if event[i] == False and event[j] == True and S[i] == S[j] and time[i] <= time[j]:
+    #                 distance = np.sqrt(sum((X[i] - X[j]) ** 2))
+    #                 F_CG_score_sum_group[int(S[i])] = F_CG_score_sum_group[int(S[i])] + max(0.0, (
+    #                         np.abs(prediction[index][i] - prediction[index][j]) - scale * distance))
+    #     if censoring_count != 0 and uncensored_count != 0:
+    #         F_CG_score_avg = np.sum(F_CG_score_sum_group) / (censoring_count * uncensored_count)
+    #     else:
+    #         F_CG_score_avg = 0.0
+    #     F_CG_score_list.append(F_CG_score_avg)
+    # F_CG_score = np.mean(np.asarray(F_CG_score_list))
+    # return F_CG_score
 
 
 # %% fairness metric: intersectional fairness
